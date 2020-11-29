5637fe622fd3b7582ff5f45900cc810fb6b5692d
gr-satt <gr-satt@protonmail.com>
2020-11-29 01:35:40 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# portopt
+
+```python
+from portopt import PortOpt
+
+portfolio = PortOpt(assets='assets.csv')
+portfolio.optimize(n=1000, rf=0.0009, startdate='2020-6-1')
+
+```
--- portopt.py
+++ portopt.py
@@ -1,4 +1,5 @@
 import csv
+from os import path
 
 import numpy as np
 import pandas as pd
@@ -21,25 +22,17 @@ def _read_csv(file):
     return tickers, invesment_names
 
 
-class Optimize:
+class PortOpt:
     """Efficient frontier analysis & asset allocation optimization"""
 
-    def __init__(self, assets: str = 'assets.csv', n: int = 1000, rf: float = 0.0009, startdate: str = '2020-6-1'):
-        """params:
-        n: # of portfolio simulations
-        rf: risk-free rate
-        startdate: year-month-day"""
-        self.n = n
-        self.rf = rf
-        self.startdate = startdate
-
+    def __init__(self, assets: str = 'assets.csv'):
         self.tickers, self.investment_names = _read_csv(assets)
 
     # daily adj. close price to dataframe
-    def _get_price_data(self):
+    def _get_price_data(self, startdate):
         data = pd.DataFrame()
         for ticker in self.tickers:
-            data[ticker] = wb.DataReader(ticker, 'yahoo', self.startdate)['Adj Close']
+            data[ticker] = wb.DataReader(ticker, 'yahoo', startdate)['Adj Close']
         return data
     
     def _calc_returns(self, data):
@@ -47,15 +40,18 @@ class Optimize:
         return returns
     
     # simulate portfolio variations
-    def _simulate(self, returns):
+    def _simulate(self, returns, n, rf):
         mean = returns.mean() * 252         # 252 trading days / yr on avg
         cov = returns.cov() * 252
         corr = returns.corr()
-        # TODO store mean, cov, corr
+        print('\n\n', corr)
+        
+        cov.to_csv('bin/covariance.csv')
+        corr.to_csv('bin/correlation.csv')
 
         ret, vol, wgt = ([] for i in range(3))
 
-        for _ in range(self.n):
+        for _ in range(n):
             weights = np.random.random(len(self.tickers))
             weights /= np.sum(weights)
 
@@ -71,7 +67,7 @@ class Optimize:
         })
 
         # calc sharpe for each portfolio (row) in dataframe
-        portfolios['sharpe'] = (portfolios['return'] - self.rf) / portfolios['volatility']
+        portfolios['sharpe'] = (portfolios['return'] - rf) / portfolios['volatility']
 
         return portfolios
     
@@ -89,15 +85,18 @@ class Optimize:
         plt.colorbar(label='Sharpe Ratio')
         plt.show()
 
-    def opt(self):
-        data = self._get_price_data()
+    def optimize(self, n: int = 1000, rf: float = 0.0009, startdate: str = '2020-6-1'):
+        """params:
+        n: # of portfolio simulations
+        rf: risk-free rate
+        startdate: year-month-day"""
+        data = self._get_price_data(startdate)
         returns = self._calc_returns(data)
-        portfolios = self._simulate(returns)
+        portfolios = self._simulate(returns, n, rf)
 
         # find optimal portfolio (max sharpe)
         opt_port = pd.DataFrame(portfolios.loc[portfolios['sharpe'] == portfolios['sharpe'].max()])
-        # TODO store opt_port
-
+        print('\n\n', opt_port)
 
         opt_weights = []
         for w in opt_port['weights']:
@@ -105,7 +104,6 @@ class Optimize:
                 opt_weights.append(f'{round(_ * 100, 2)} %')
         opt_weights = list(zip(self.tickers, self.investment_names, opt_weights))
         opt = pd.DataFrame(opt_weights, columns=['ticker', 'investment name', 'allocation'])
-        # TODO store opt
-        print(opt)
+        print('\n\n', opt)
            
         self._plot(portfolios)
